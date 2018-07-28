f1143e4872465c328d8ceeb7fb3023daed2ec13e
Jose Lidier <lidiera@gmail.com>
2018-07-28 00:39:48 +0000

    Upsert Merge backup

---
+++ bi-homework-bids2008/sql-scripts/sProc_UpsertMerge.sql
@@ -0,0 +1,152 @@
+-- ================================================
+-- Template generated from Template Explorer using:
+-- Create Procedure (New Menu).SQL
+--
+-- Use the Specify Values for Template Parameters 
+-- command (Ctrl-Shift-M) to fill in the parameter 
+-- values below.
+--
+-- This block of comments will not be included in
+-- the definition of the procedure.
+-- ================================================
+SET ANSI_NULLS ON
+GO
+SET QUOTED_IDENTIFIER ON
+GO
+-- =============================================
+-- Author:		<Author,,Name>
+-- Create date: <Create Date,,>
+-- Description:	<Description,,>
+-- =============================================
+ALTER PROCEDURE sProc_UpsertMerge
+	-- Add the parameters for the stored procedure here
+	--@SourceTable nvarchar(50) = NULL, 
+	--@PrimaryKeyColumn nvarchar(50) = NULL,
+	--@DestinationTable nvarchar(50) = NULL 
+	@SourceTable nvarchar(50) = '[TEMP_Source_DataIn_Account]', 
+	@PrimaryKeyColumn nvarchar(50) = '[Account No]',
+	@DestinationTable nvarchar(50) = '[Source_DataIn_Account]' 
+	
+AS
+BEGIN
+	-- SET NOCOUNT ON added to prevent extra result sets from
+	-- interfering with SELECT statements.
+	SET NOCOUNT ON;
+
+    -- Insert statements for procedure here
+
+	---------------------------------------------------------------------------
+	-- Detect all table columns except primary key
+	---------------------------------------------------------------------------
+	DECLARE @ColumnsString AS nvarchar(1000);
+	
+	WITH column_names AS (
+		SELECT 
+			column_name 
+		FROM [information_schema].[columns] 
+		WHERE table_name = @DestinationTable
+		AND column_name <> SUBSTRING(@PrimaryKeyColumn, 2,(LEN(@PrimaryKeyColumn) -2))
+	),
+	column_names_subresult AS (
+		SELECT TOP 1 
+			SUBSTRING(
+				(
+				SELECT ',['+ column_names.column_name  + ']' AS [text()]
+				FROM column_names
+				FOR XML PATH ('')
+				)
+			, 2, 1000) AS [columns]
+		FROM column_names)
+	SELECT @ColumnsString = [columns] FROM column_names_subresult
+
+	PRINT (@ColumnsString)
+	
+
+
+	---------------------------------------------------------------------------
+	-- Insert new rows
+	---------------------------------------------------------------------------
+
+	DECLARE @SQL VARCHAR(MAX);
+
+	SET @SQL = '
+	INSERT ' + @DestinationTable + ' (' + @PrimaryKeyColumn + ',' +  @ColumnsString + ')  
+	SELECT ' + @PrimaryKeyColumn + ',' + @ColumnsString + '   
+	FROM ' + @SourceTable + ' AS SourceTable  
+	WHERE NOT EXISTS (
+		SELECT ' + @PrimaryKeyColumn + '
+		FROM ' + @DestinationTable + ' AS DestinationTable
+		WHERE DestinationTable.' + @PrimaryKeyColumn + '= SourceTable.' + @PrimaryKeyColumn + '
+	)'
+	
+	PRINT(@SQL);
+	EXEC(@SQL);
+
+	---------------------------------------------------------------------------
+	-- Update existing rows
+	---------------------------------------------------------------------------
+
+	DECLARE Columns_Cursor CURSOR FOR 
+    SELECT '[' + column_name + ']'
+	FROM [information_schema].[columns] 
+	WHERE table_name = @DestinationTable
+	AND column_name <> SUBSTRING(@PrimaryKeyColumn, 2,(LEN(@PrimaryKeyColumn) -2))
+
+	OPEN Columns_Cursor 
+	FETCH NEXT FROM Columns_Cursor INTO @CursorColumnName
+	WHILE @@FETCH_STATUS = 0
+	BEGIN
+		SET @SQL = '
+			UPDATE ' + @DestinationTable + '
+			SET ' + @CursorColumnName + ' = ''
+			FROM ' + @DestinationTable + ' AS [Destination]
+			INNER JOIN ' + @SourceTable + ' AS [Source]
+			ON [Destination].' + @PrimaryKeyColumn + ' = [Source].' + @PrimaryKeyColumn + '
+			WHERE 
+	
+	UPDATE ProductReviews
+SET    ProductReviews.status = '0'
+FROM   ProductReviews
+       INNER JOIN products
+         ON ProductReviews.pid = products.id
+WHERE  ProductReviews.id = '17190'
+       AND products.shopkeeper = '89137'
+
+
+
+    FETCH NEXT FROM Customer_Cursor INTO
+    @CustomerId, @FirstName, @LastName
+END
+CLOSE Customer_Cursor
+DEALLOCATE Customer_Cursor
+
+
+
+	--SET @SQL = '
+	--INSERT ' + @DestinationTable + ' (' + @PrimaryKeyColumn + ',' +  @ColumnsString + ')  
+	--SELECT ' + @PrimaryKeyColumn + ',' + @ColumnsString + '   
+	--FROM ' + @SourceTable + ' AS SourceTable  
+	--WHERE NOT EXISTS (
+	--	SELECT ' + @PrimaryKeyColumn + '
+	--	FROM ' + @DestinationTable + ' AS DestinationTable
+	--	WHERE DestinationTable.' + @PrimaryKeyColumn + '= SourceTable.' + @PrimaryKeyColumn + '
+	--)'
+
+	--SET @SQL = '
+	--MERGE INTO ' + @DestinationTable + '  AS DestinationTable  
+ --   USING ' + @SourceTable + '  
+ --   ON  DestinationTable.' + + '
+ --   [ WHEN MATCHED [ AND <clause_search_condition> ]  
+ --       THEN <merge_matched> ] [ ...n ]  
+ --   [ WHEN NOT MATCHED [ BY TARGET ] [ AND <clause_search_condition> ]  
+ --       THEN <merge_not_matched> ]  
+ --   [ WHEN NOT MATCHED BY SOURCE [ AND <clause_search_condition> ]  
+ --       THEN <merge_matched> ] [ ...n ]  
+ --   [ <output_clause> ]  
+ --   [ OPTION ( <query_hint> [ ,...n ] ) ]  
+--	' 
+	
+	PRINT(@SQL);
+	EXEC(@SQL);
+END
+GO
